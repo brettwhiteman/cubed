acf59d7daba5b31037901ecfd802b65dbc380e46
Brett Whiteman <msea@gmx.com>
2015-11-27 14:30:51 +1300

    use proper C++ sleep

--- cubed_client/src/game.cpp
+++ cubed_client/src/game.cpp
@@ -1,6 +1,6 @@
 #include "game.h"
 #include <chrono>
-#include <Windows.h>
+#include <thread>
 #include "world_gen/world_gen.h"
 
 Game::Game() :
@@ -49,7 +49,7 @@ void Game::run()
 		}
 		else
 		{
-			Sleep(1);
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		}
 	}
 }
